48ca53f632fa9b18dc27852ba3e88f74110f1252
Akshithaa123 <144244351+Akshithaa123@users.noreply.github.com>
2023-12-06 16:09:01 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,4 @@
+function toggleContactInfo() {
+    var contactInfo = document.getElementById('contactInfo');
+    contactInfo.style.display = (contactInfo.style.display === 'block') ? 'none' : 'block';
+}
